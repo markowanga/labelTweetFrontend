0399c71b4816e409a45f4eccb76f807c8e71155f
Marcin Wątroba <marcin.watroba@deviniti.com>
2019-12-02 03:03:58 +0100

    Layout changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {LabeledTweetComponent} from './labeled-tweet/labeled-tweet.component';
 import {
   MatInputModule, MatPaginatorModule, MatProgressSpinnerModule,
-  MatSortModule, MatTableModule
+  MatSortModule, MatTableModule, MatButtonModule
 } from '@angular/material';
 import { AddLabelComponent } from './add-label/add-label.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -27,6 +27,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatPaginatorModule,
     MatProgressSpinnerModule,
+    MatButtonModule,
     MatSortModule,
     MatTableModule,
     ReactiveFormsModule,
